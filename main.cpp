28c83347aca061e829e04491502245a7184142dd
diyooor <diyorsattarov7@gmail.com>
2024-01-17 02:47:25 -0600

    Removed mime type and path_cat, implemented count_char helper function to assist with code readability

--- main.cpp
+++ main.cpp
@@ -24,12 +24,6 @@ namespace http = beast::http;
 namespace net = boost::asio;
 using tcp = boost::asio::ip::tcp;
 
-class SessionData {
-public:
-  std::string username;
-  std::chrono::steady_clock::time_point expiration_time;
-};
-
 class PgConnectionPool {
 public:
   PgConnectionPool(const std::string &conn_str, size_t pool_size)
@@ -72,79 +66,12 @@ public:
 private:
   PgConnectionPool &pg_pool_;
 };
-beast::string_view mime_type(beast::string_view path) {
-  using beast::iequals;
-  auto const ext = [&path] {
-    auto const pos = path.rfind(".");
-    if (pos == beast::string_view::npos)
-      return beast::string_view{};
-    return path.substr(pos);
-  }();
-  if (iequals(ext, ".htm"))
-    return "text/html";
-  if (iequals(ext, ".html"))
-    return "text/html";
-  if (iequals(ext, ".php"))
-    return "text/html";
-  if (iequals(ext, ".css"))
-    return "text/css";
-  if (iequals(ext, ".txt"))
-    return "text/plain";
-  if (iequals(ext, ".js"))
-    return "application/javascript";
-  if (iequals(ext, ".json"))
-    return "application/json";
-  if (iequals(ext, ".xml"))
-    return "application/xml";
-  if (iequals(ext, ".swf"))
-    return "application/x-shockwave-flash";
-  if (iequals(ext, ".flv"))
-    return "video/x-flv";
-  if (iequals(ext, ".png"))
-    return "image/png";
-  if (iequals(ext, ".jpe"))
-    return "image/jpeg";
-  if (iequals(ext, ".jpeg"))
-    return "image/jpeg";
-  if (iequals(ext, ".jpg"))
-    return "image/jpeg";
-  if (iequals(ext, ".gif"))
-    return "image/gif";
-  if (iequals(ext, ".bmp"))
-    return "image/bmp";
-  if (iequals(ext, ".ico"))
-    return "image/vnd.microsoft.icon";
-  if (iequals(ext, ".tiff"))
-    return "image/tiff";
-  if (iequals(ext, ".tif"))
-    return "image/tiff";
-  if (iequals(ext, ".svg"))
-    return "image/svg+xml";
-  if (iequals(ext, ".svgz"))
-    return "image/svg+xml";
-  return "application/text";
-}
 
-std::string path_cat(beast::string_view base, beast::string_view path) {
-  if (base.empty())
-    return std::string(path);
-  std::string result(base);
-#ifdef BOOST_MSVC
-  char constexpr path_separator = '\\';
-  if (result.back() == path_separator)
-    result.resize(result.size() - 1);
-  result.append(path.data(), path.size());
-  for (auto &c : result)
-    if (c == '/')
-      c = path_separator;
-#else
-  char constexpr path_separator = '/';
-  if (result.back() == path_separator)
-    result.resize(result.size() - 1);
-  result.append(path.data(), path.size());
-#endif
-  return result;
-}
+class SessionData {
+public:
+  std::string username;
+  std::chrono::steady_clock::time_point expiration_time;
+};
 
 bool validate_token(
     const std::string &token,
@@ -154,19 +81,20 @@ bool validate_token(
          session_it->second.expiration_time > std::chrono::steady_clock::now();
 }
 
+size_t count_char(const std::string &target, char ch) {
+  return std::count(target.begin(), target.end(), ch);
+}
+
 template <class Body, class Allocator>
 http::message_generator
-handle_request(beast::string_view doc_root,
-               http::request<Body, http::basic_fields<Allocator>> &&req,
+handle_request(http::request<Body, http::basic_fields<Allocator>> &&req,
                PgConnectionPool &pg_pool,
                std::unordered_map<std::string, SessionData> &session_storage,
                queryAbstraction &query_abstraction) {
-  size_t num_slashes =
-      std::count(req.target().begin(), req.target().end(), '/');
-  size_t num_questions =
-      std::count(req.target().begin(), req.target().end(), '?');
-  size_t num_equals = std::count(req.target().begin(), req.target().end(), '=');
-  size_t num_ands = std::count(req.target().begin(), req.target().end(), '&');
+  size_t num_slashes = count_char(req.target(), '/');
+  size_t num_questions = count_char(req.target(), '?');
+  size_t num_equals = count_char(req.target(), '=');
+  size_t num_ands = count_char(req.target(), '&');
 
   auto const login_api = [&req, &pg_pool, &session_storage,
                           &query_abstraction](const std::vector<std::string>
@@ -175,14 +103,11 @@ handle_request(beast::string_view doc_root,
       return http::response<http::string_body>{http::status::bad_request,
                                                req.version()};
     }
-
     const std::string &username = username_password[0];
     const std::string &password = username_password[1];
-
     std::string query = "SELECT * FROM mock_data WHERE username = '" +
                         username + "' AND password = '" + password + "';";
     pqxx::result result = query_abstraction.executeSelect(query);
-
     if (!result.empty()) {
       http::response<http::string_body> res{http::status::ok, req.version()};
       std::string session_id =
@@ -196,7 +121,6 @@ handle_request(beast::string_view doc_root,
       session_data.username = username;
       session_data.expiration_time =
           std::chrono::steady_clock::now() + std::chrono::minutes(30);
-
       session_storage[session_id] = session_data;
       res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
       res.set(http::field::content_type, "text/plain");
@@ -205,16 +129,15 @@ handle_request(beast::string_view doc_root,
       res.set(http::field::authorization, "Bearer " + session_id);
       res.prepare_payload();
       return res;
-    } else {
-      http::response<http::string_body> res{http::status::unauthorized,
-                                            req.version()};
-      res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-      res.set(http::field::content_type, "text/plain");
-      res.keep_alive(req.keep_alive());
-      res.body() = "Authentication failed";
-      res.prepare_payload();
-      return res;
     }
+    http::response<http::string_body> res{http::status::unauthorized,
+                                          req.version()};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "text/plain");
+    res.keep_alive(req.keep_alive());
+    res.body() = "Authentication failed";
+    res.prepare_payload();
+    return res;
   };
   auto const unauthenticated_route = [&req, &pg_pool, &session_storage,
                                       &query_abstraction](
@@ -223,11 +146,9 @@ handle_request(beast::string_view doc_root,
     res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
     res.set(http::field::content_type, "application/json");
     res.keep_alive(req.keep_alive());
-
     res.prepare_payload();
     return res;
   };
-
   auto const authenticated_route = [&req, &pg_pool, &session_storage,
                                     &query_abstraction](
                                        beast::string_view target) {
@@ -266,7 +187,6 @@ handle_request(beast::string_view doc_root,
     res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
     res.set(http::field::content_type, "application/json");
     res.keep_alive(req.keep_alive());
-
     res.prepare_payload();
     return res;
   };
@@ -303,6 +223,7 @@ handle_request(beast::string_view doc_root,
     res.prepare_payload();
     return res;
   };
+
   if (req.method() == http::verb::post) {
     if (req.target().starts_with("/api?=login")) {
       std::string target = std::string(req.target());
@@ -331,7 +252,6 @@ handle_request(beast::string_view doc_root,
       req.target().find("..") != beast::string_view::npos)
     return bad_request("Illegal request-target");
 
-  std::string path = path_cat(doc_root, req.target());
   if (req.target().back() == '/' && req.target().size() == 1)
     return unauthenticated_route(req.target());
   if (num_slashes == 1 && num_questions == 1) {
@@ -347,25 +267,6 @@ handle_request(beast::string_view doc_root,
   } else {
     return unauthenticated_route(req.target());
   }
-
-  beast::error_code ec;
-  http::file_body::value_type body;
-  body.open(path.c_str(), beast::file_mode::scan, ec);
-
-  if (ec == beast::errc::no_such_file_or_directory)
-    return not_found(req.target());
-
-  if (ec)
-    return server_error(ec.message());
-
-  auto const size = body.size();
-  http::response<http::file_body> res{
-      std::piecewise_construct, std::make_tuple(std::move(body)),
-      std::make_tuple(http::status::ok, req.version())};
-  res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-  res.content_length(size);
-  res.keep_alive(req.keep_alive());
-  return res;
 }
 
 void fail(beast::error_code ec, char const *what) {
@@ -375,19 +276,16 @@ void fail(beast::error_code ec, char const *what) {
 class session : public std::enable_shared_from_this<session> {
   beast::tcp_stream stream_;
   beast::flat_buffer buffer_;
-  std::shared_ptr<std::string const> doc_root_;
   http::request<http::string_body> req_;
   PgConnectionPool &pg_pool_;
   std::unordered_map<std::string, SessionData> session_storage_;
   queryAbstraction &query_abstraction_;
 
 public:
-  session(tcp::socket &&socket,
-          std::shared_ptr<std::string const> const &doc_root,
-          PgConnectionPool &pg_pool,
+  session(tcp::socket &&socket, PgConnectionPool &pg_pool,
           std::unordered_map<std::string, SessionData> &session_storage,
           queryAbstraction &query_abstraction)
-      : stream_(std::move(socket)), doc_root_(doc_root), pg_pool_(pg_pool),
+      : stream_(std::move(socket)), pg_pool_(pg_pool),
         session_storage_(session_storage),
         query_abstraction_(query_abstraction) {}
 
@@ -416,8 +314,8 @@ public:
     if (ec)
       return fail(ec, "read");
 
-    send_response(handle_request(*doc_root_, std::move(req_), pg_pool_,
-                                 session_storage_, query_abstraction_));
+    send_response(handle_request(std::move(req_), pg_pool_, session_storage_,
+                                 query_abstraction_));
   }
 
   void send_response(http::message_generator &&msg) {
@@ -447,22 +345,21 @@ public:
     stream_.socket().shutdown(tcp::socket::shutdown_send, ec);
   }
 };
+
 class listener : public std::enable_shared_from_this<listener> {
   net::io_context &ioc_;
   tcp::acceptor acceptor_;
-  std::shared_ptr<std::string const> doc_root_;
   PgConnectionPool &pg_pool_;
   std::unordered_map<std::string, SessionData> session_storage_;
   queryAbstraction &query_abstraction_;
 
 public:
   listener(net::io_context &ioc, tcp::endpoint endpoint,
-           std::shared_ptr<std::string const> const &doc_root,
            PgConnectionPool &pg_pool,
            std::unordered_map<std::string, SessionData> &session_storage,
            queryAbstraction &query_abstraction)
-      : ioc_(ioc), acceptor_(net::make_strand(ioc)), doc_root_(doc_root),
-        pg_pool_(pg_pool), session_storage_(session_storage),
+      : ioc_(ioc), acceptor_(net::make_strand(ioc)), pg_pool_(pg_pool),
+        session_storage_(session_storage),
         query_abstraction_(query_abstraction) {
     beast::error_code ec;
 
@@ -505,8 +402,8 @@ private:
       fail(ec, "accept");
       return;
     } else {
-      std::make_shared<session>(std::move(socket), doc_root_, pg_pool_,
-                                session_storage_, query_abstraction_)
+      std::make_shared<session>(std::move(socket), pg_pool_, session_storage_,
+                                query_abstraction_)
           ->run();
     }
 
@@ -515,11 +412,10 @@ private:
 };
 
 int main(int argc, char *argv[]) {
-  if (argc != 5) {
-    std::cerr
-        << "Usage: http-server-async <address> <port> <doc_root> <threads>\n"
-        << "Example:\n"
-        << "    http-server-async 0.0.0.0 8080 . 1\n";
+  if (argc != 4) {
+    std::cerr << "Usage: http-server-async <address> <port> <threads>\n"
+              << "Example:\n"
+              << "    http-server-async 0.0.0.0 8080 . 1\n";
     return EXIT_FAILURE;
   }
   PgConnectionPool pg_pool("dbname=database_2024 user=user "
@@ -528,13 +424,12 @@ int main(int argc, char *argv[]) {
   std::unordered_map<std::string, SessionData> session_storage;
   auto const address = net::ip::make_address(argv[1]);
   auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
-  auto const doc_root = std::make_shared<std::string>(argv[3]);
-  auto const threads = std::max<int>(1, std::atoi(argv[4]));
+  auto const threads = std::max<int>(1, std::atoi(argv[3]));
   queryAbstraction query_abstraction(pg_pool);
   net::io_context ioc{threads};
 
-  std::make_shared<listener>(ioc, tcp::endpoint{address, port}, doc_root,
-                             pg_pool, session_storage, query_abstraction)
+  std::make_shared<listener>(ioc, tcp::endpoint{address, port}, pg_pool,
+                             session_storage, query_abstraction)
       ->run();
 
   std::vector<std::thread> v;
