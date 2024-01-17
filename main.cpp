568ba017744e9482360fdb90806a836a079f8215
diyooor <diyorsattarov7@gmail.com>
2024-01-17 03:09:51 -0600

    Implemented duplicate session validation and updated unauthorized route status code from unknown to unauthorized

--- main.cpp
+++ main.cpp
@@ -17,13 +17,11 @@
 #include <thread>
 #include <unordered_map>
 #include <vector>
-
 using json = nlohmann::json;
 namespace beast = boost::beast;
 namespace http = beast::http;
 namespace net = boost::asio;
 using tcp = boost::asio::ip::tcp;
-
 class PgConnectionPool {
 public:
   PgConnectionPool(const std::string &conn_str, size_t pool_size)
@@ -33,7 +31,6 @@ public:
       connections_.emplace_back(connection);
     }
   }
-
   std::shared_ptr<pqxx::connection> get_connection() {
     std::cout << "get_connection()" << std::endl;
     return connections_.front();
@@ -44,11 +41,9 @@ private:
   size_t pool_size_;
   std::vector<std::shared_ptr<pqxx::connection>> connections_;
 };
-
 class queryAbstraction {
 public:
   queryAbstraction(PgConnectionPool &pg_pool) : pg_pool_(pg_pool) {}
-
   pqxx::result executeSelect(const std::string &query) {
     try {
       auto connection = pg_pool_.get_connection();
@@ -66,13 +61,11 @@ public:
 private:
   PgConnectionPool &pg_pool_;
 };
-
 class SessionData {
 public:
   std::string username;
   std::chrono::steady_clock::time_point expiration_time;
 };
-
 bool validate_token(
     const std::string &token,
     const std::unordered_map<std::string, SessionData> &session_storage) {
@@ -80,11 +73,9 @@ bool validate_token(
   return session_it != session_storage.end() &&
          session_it->second.expiration_time > std::chrono::steady_clock::now();
 }
-
 size_t count_char(const std::string &target, char ch) {
   return std::count(target.begin(), target.end(), ch);
 }
-
 template <class Body, class Allocator>
 http::message_generator
 handle_request(http::request<Body, http::basic_fields<Allocator>> &&req,
@@ -117,6 +108,13 @@ handle_request(http::request<Body, http::basic_fields<Allocator>> &&req,
       std::cout << "login_api session id: " << session_id << std::endl
                 << "login_api session_storage size: " << session_storage.size()
                 << std::endl;
+      auto existing_session_it =
+          std::find_if(session_storage.begin(), session_storage.end(),
+                       [&username](const auto &session_pair) {
+                         return session_pair.second.username == username;
+                       });
+      if (existing_session_it != session_storage.end())
+        session_storage.erase(existing_session_it);
       SessionData session_data;
       session_data.username = username;
       session_data.expiration_time =
@@ -139,16 +137,17 @@ handle_request(http::request<Body, http::basic_fields<Allocator>> &&req,
     res.prepare_payload();
     return res;
   };
-  auto const unauthenticated_route = [&req, &pg_pool, &session_storage,
-                                      &query_abstraction](
-                                         beast::string_view target) {
-    http::response<http::string_body> res{http::status::unknown, req.version()};
-    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-    res.set(http::field::content_type, "application/json");
-    res.keep_alive(req.keep_alive());
-    res.prepare_payload();
-    return res;
-  };
+  auto const unauthenticated_route =
+      [&req, &pg_pool, &session_storage,
+       &query_abstraction](beast::string_view target) {
+        http::response<http::string_body> res{http::status::unauthorized,
+                                              req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, "application/json");
+        res.keep_alive(req.keep_alive());
+        res.prepare_payload();
+        return res;
+      };
   auto const authenticated_route = [&req, &pg_pool, &session_storage,
                                     &query_abstraction](
                                        beast::string_view target) {
@@ -190,7 +189,6 @@ handle_request(http::request<Body, http::basic_fields<Allocator>> &&req,
     res.prepare_payload();
     return res;
   };
-
   auto const bad_request = [&req](beast::string_view why) {
     http::response<http::string_body> res{http::status::bad_request,
                                           req.version()};
@@ -201,7 +199,6 @@ handle_request(http::request<Body, http::basic_fields<Allocator>> &&req,
     res.prepare_payload();
     return res;
   };
-
   auto const not_found = [&req](beast::string_view target) {
     http::response<http::string_body> res{http::status::not_found,
                                           req.version()};
@@ -212,7 +209,6 @@ handle_request(http::request<Body, http::basic_fields<Allocator>> &&req,
     res.prepare_payload();
     return res;
   };
-
   auto const server_error = [&req](beast::string_view what) {
     http::response<http::string_body> res{http::status::internal_server_error,
                                           req.version()};
@@ -223,14 +219,11 @@ handle_request(http::request<Body, http::basic_fields<Allocator>> &&req,
     res.prepare_payload();
     return res;
   };
-
   if (req.method() == http::verb::post) {
     if (req.target().starts_with("/api?=login")) {
       std::string target = std::string(req.target());
-
       size_t username_pos = target.find("&username=");
       size_t password_pos = target.find("&password=");
-
       if (username_pos != std::string::npos &&
           password_pos != std::string::npos) {
         std::string username = target.substr(
@@ -247,32 +240,26 @@ handle_request(http::request<Body, http::basic_fields<Allocator>> &&req,
   }
   if (req.method() != http::verb::get && req.method() != http::verb::head)
     return bad_request("Unknown HTTP-method");
-
   if (req.target().empty() || req.target()[0] != '/' ||
       req.target().find("..") != beast::string_view::npos)
     return bad_request("Illegal request-target");
-
   if (req.target().back() == '/' && req.target().size() == 1)
     return unauthenticated_route(req.target());
   if (num_slashes == 1 && num_questions == 1) {
-    if (num_equals == 0) {
+    if (num_equals == 0)
       return bad_request("Illegal request-target");
-    }
     if (req.target() == "/api?=authenticated_products" &&
-        req.method() == http::verb::get) {
+        req.method() == http::verb::get)
       return authenticated_route(req.target());
-    } else {
+    else
       return bad_request("Illegal request-target");
-    }
   } else {
     return unauthenticated_route(req.target());
   }
 }
-
 void fail(beast::error_code ec, char const *what) {
   std::cerr << what << ": " << ec.message() << "\n";
 }
-
 class session : public std::enable_shared_from_this<session> {
   beast::tcp_stream stream_;
   beast::flat_buffer buffer_;
@@ -288,64 +275,48 @@ public:
       : stream_(std::move(socket)), pg_pool_(pg_pool),
         session_storage_(session_storage),
         query_abstraction_(query_abstraction) {}
-
   void run() {
     net::dispatch(
         stream_.get_executor(),
         beast::bind_front_handler(&session::do_read, shared_from_this()));
   }
-
   void do_read() {
     req_ = {};
-
     stream_.expires_after(std::chrono::seconds(30));
-
     http::async_read(
         stream_, buffer_, req_,
         beast::bind_front_handler(&session::on_read, shared_from_this()));
   }
-
   void on_read(beast::error_code ec, std::size_t bytes_transferred) {
     boost::ignore_unused(bytes_transferred);
-
     if (ec == http::error::end_of_stream)
       return do_close();
-
     if (ec)
       return fail(ec, "read");
-
     send_response(handle_request(std::move(req_), pg_pool_, session_storage_,
                                  query_abstraction_));
   }
-
   void send_response(http::message_generator &&msg) {
     bool keep_alive = msg.keep_alive();
-
     beast::async_write(stream_, std::move(msg),
                        beast::bind_front_handler(
                            &session::on_write, shared_from_this(), keep_alive));
   }
-
   void on_write(bool keep_alive, beast::error_code ec,
                 std::size_t bytes_transferred) {
     boost::ignore_unused(bytes_transferred);
-
     if (ec)
       return fail(ec, "write");
-
     if (!keep_alive) {
       return do_close();
     }
-
     do_read();
   }
-
   void do_close() {
     beast::error_code ec;
     stream_.socket().shutdown(tcp::socket::shutdown_send, ec);
   }
 };
-
 class listener : public std::enable_shared_from_this<listener> {
   net::io_context &ioc_;
   tcp::acceptor acceptor_;
@@ -362,32 +333,27 @@ public:
         session_storage_(session_storage),
         query_abstraction_(query_abstraction) {
     beast::error_code ec;
-
     acceptor_.open(endpoint.protocol(), ec);
     if (ec) {
       fail(ec, "open");
       return;
     }
-
     acceptor_.set_option(net::socket_base::reuse_address(true), ec);
     if (ec) {
       fail(ec, "set_option");
       return;
     }
-
     acceptor_.bind(endpoint, ec);
     if (ec) {
       fail(ec, "bind");
       return;
     }
-
     acceptor_.listen(net::socket_base::max_listen_connections, ec);
     if (ec) {
       fail(ec, "listen");
       return;
     }
   }
-
   void run() { do_accept(); }
 
 private:
@@ -396,7 +362,6 @@ private:
         net::make_strand(ioc_),
         beast::bind_front_handler(&listener::on_accept, shared_from_this()));
   }
-
   void on_accept(beast::error_code ec, tcp::socket socket) {
     if (ec) {
       fail(ec, "accept");
@@ -406,11 +371,9 @@ private:
                                 query_abstraction_)
           ->run();
     }
-
     do_accept();
   }
 };
-
 int main(int argc, char *argv[]) {
   if (argc != 4) {
     std::cerr << "Usage: http-server-async <address> <port> <threads>\n"
@@ -427,16 +390,13 @@ int main(int argc, char *argv[]) {
   auto const threads = std::max<int>(1, std::atoi(argv[3]));
   queryAbstraction query_abstraction(pg_pool);
   net::io_context ioc{threads};
-
   std::make_shared<listener>(ioc, tcp::endpoint{address, port}, pg_pool,
                              session_storage, query_abstraction)
       ->run();
-
   std::vector<std::thread> v;
   v.reserve(threads - 1);
   for (auto i = threads - 1; i > 0; --i)
     v.emplace_back([&ioc] { ioc.run(); });
   ioc.run();
-
   return EXIT_SUCCESS;
 }
