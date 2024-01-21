66dc3e8b85a6f17be384d5782e516d2d564c1e44
diyooor <diyorsattarov7@gmail.com>
2024-01-21 01:56:56 -0600

    Deprecated validate_token method

--- src/handler.cpp
+++ src/handler.cpp
@@ -1,14 +1,6 @@
 #include "../include/handler.hpp"
 #include <exception>
 
-bool validate_token(
-    const std::string &token,
-    const std::unordered_map<std::string, SessionData> &session_storage) {
-  auto session_it = session_storage.find(token);
-  return session_it != session_storage.end() &&
-         session_it->second.expiration_time > std::chrono::steady_clock::now();
-}
-
 template <class Body, class Allocator>
 http::message_generator
 handle_request(http::request<Body, http::basic_fields<Allocator>> &&req,
