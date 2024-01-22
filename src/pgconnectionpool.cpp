a9aa9dbf1b1a634257c20e972689f2f030766948
diyooor <diyorsattarov7@gmail.com>
2024-01-21 23:01:02 -0600

    Updated log levels, messages, implemented session storage

--- src/pgconnectionpool.cpp
+++ src/pgconnectionpool.cpp
@@ -7,8 +7,7 @@
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
-std::string generateSession() {
-  // Generate a random session token (you may need to customize this)
+std::string PgConnectionPool::generateSession() {
   const std::string charset =
       "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   std::random_device rd;
@@ -19,15 +18,12 @@ std::string generateSession() {
   for (int i = 0; i < 16; ++i) {
     token += charset[distribution(generator)];
   }
-
   return token;
 }
-
 PgConnectionPool::PgConnectionPool(const std::string &conn_str,
                                    size_t pool_size, const dbSchema_ &schema)
     : conn_str_(conn_str), pool_size_(pool_size), schema_(schema) {
   auto console_logger = spdlog::stdout_color_mt("console_logger");
-  // auto file_logger = spdlog::basic_logger_mt("file_logger", "logfile.txt");
   console_logger->set_level(spdlog::level::trace);
   spdlog::set_default_logger(console_logger);
   spdlog::flush_every(std::chrono::seconds(3));
@@ -36,18 +32,28 @@ PgConnectionPool::PgConnectionPool(const std::string &conn_str,
     connections_.emplace_back(connection);
   }
 }
-
 std::shared_ptr<pqxx::connection> PgConnectionPool::get_connection() {
-  spdlog::get("console_logger")->debug("PgConnectionPool::get_connection");
+  spdlog::get("console_logger")->info("get_connection");
   return connections_.front();
 }
-
 void PgConnectionPool::selectQuery() {}
-
+pqxx::result PgConnectionPool::selectQuery(const std::string &table) {
+  spdlog::get("console_logger")->info("selectQuery");
+  try {
+    auto connection = get_connection();
+    pqxx::work transaction(*connection);
+    pqxx::result result = transaction.exec_params("", "");
+    transaction.commit();
+    return result;
+  } catch (const std::exception &e) {
+    spdlog::get("console_logger")
+        ->error("Exception acught in selectQuery: {}", e.what());
+    throw e;
+  }
+}
 bool PgConnectionPool::selectQuery(const std::string &table,
                                    const std::string &username) {
-  spdlog::get("console_logger")
-      ->debug("bool PgConnectionPool::selectQuery (username validation)");
+  spdlog::get("console_logger")->info("selectQuery {}", username);
   const std::string query = "SELECT * FROM " + table + " WHERE " +
                             schema_.db_schema.at(table)[0] + " = $1";
   try {
@@ -58,51 +64,43 @@ bool PgConnectionPool::selectQuery(const std::string &table,
     return !result.empty();
   } catch (const std::exception &e) {
     spdlog::error("Exception caught in selectQuery: {}", e.what());
-    throw;
+    throw e;
   }
 }
-
 bool PgConnectionPool::selectQuery(const std::string &table,
                                    const std::string &username,
                                    const std::string &password) {
-  spdlog::get("console_logger")
-      ->debug(
-          "bool PgConnectionPool::selectQuery (username, password validation)");
+  spdlog::get("console_logger")->info("selectQuery");
   const std::string query = "SELECT * FROM " + table + " WHERE " +
                             schema_.db_schema.at(table)[0] + " = $1 AND " +
                             schema_.db_schema.at(table)[1] + " = $2";
+
   try {
     auto connection = get_connection();
     pqxx::work transaction(*connection);
     pqxx::result result = transaction.exec_params(query, username, password);
-
     if (!result.empty()) {
-      // Check if there is an active session for the user
       auto sessionIt = session_storage.find(username);
       if (sessionIt != session_storage.end()) {
         auto currentTime = std::chrono::steady_clock::now();
         if (currentTime < sessionIt->second.expiration_time) {
           spdlog::get("console_logger")
-              ->debug("Login failed. Active session exists for user: {}",
-                      username);
-          return false; // Login fails due to an active session
+              ->info("Login failed. Active session exists for user: {}",
+                     username);
+          return false;
         }
       }
-
-      // Create session on successful login
-      std::string sessionToken = generateSession();
-      session_storage[username] = {
-          username, std::chrono::steady_clock::now() +
-                        std::chrono::hours(
-                            1)}; // Set expiration time to 1 hour for example
+      std::string sessionToken = this->generateSession();
+      session_storage[username] = {username, std::chrono::steady_clock::now() +
+                                                 std::chrono::hours(1)};
       spdlog::get("console_logger")
-          ->debug("Session created for user: {}", username);
+          ->info("Session created for user: {}", username);
     }
-
     transaction.commit();
     return !result.empty();
   } catch (const std::exception &e) {
-    spdlog::error("Exception caught in selectQuery: {}", e.what());
+    spdlog::get("console_logger")
+        ->error("Exception caught in selectQuery: {}", e.what());
     return false;
   }
 }
@@ -110,11 +108,10 @@ bool PgConnectionPool::selectQuery(const std::string &table,
 bool PgConnectionPool::insertQuery(const std::string &table,
                                    const std::string &username,
                                    const std::string &password) {
-  spdlog::get("console_logger")
-      ->debug("bool PgConnectionPool::insertQuery (username, password "
-              "registration)");
-  if (selectQuery("example_table", username)) {
-    spdlog::error("Exception caught in insertQuery : Username already exists");
+  spdlog::get("console_logger")->info("insertQuery");
+  if (selectQuery("user_table", username)) {
+    spdlog::get("console_logger")
+        ->error("Exception caught in insertQuery: Username already exists");
     return false;
   }
   const std::string query =
@@ -127,7 +124,9 @@ bool PgConnectionPool::insertQuery(const std::string &table,
     transaction.commit();
     return true;
   } catch (const std::exception &e) {
-    spdlog::error("Exception caught in insertQuery: {}", e.what());
-    return false;
+    spdlog::get("console_logger")
+        ->error("Exception caught in insertQuery: {}", e.what());
+    throw e;
   }
+  return false;
 }
