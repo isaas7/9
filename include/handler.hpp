6ba5056ff7a252f9335abf5ff9bceeee2d64a2da
diyooor <diyorsattarov7@gmail.com>
2024-01-25 11:34:33 -0600

    Removed unused include directives and spdlog to util

--- include/handler.hpp
+++ include/handler.hpp
@@ -1,23 +1,8 @@
 #ifndef HANDLER_HPP
 #define HANDLER_HPP
-#include "beast.hpp"
-#include "net.hpp"
-#include <algorithm>
-#include <chrono>
-#include <cstdlib>
-#include <exception>
-#include <functional>
-#include <iostream>
-#include <memory>
 #include <nlohmann/json.hpp>
-#include <spdlog/common.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/spdlog.h>
-#include <string>
-#include <thread>
-#include <unordered_map>
-#include <vector>
+#include "beast.hpp"
+#include "util.hpp"
 using json = nlohmann::json;
 
 template <class Body, class Allocator>
